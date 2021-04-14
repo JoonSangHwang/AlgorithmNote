5b99d901d1a9002f089d270c701ce4608b87dd72
joonsang <gufrus@naver.com>
2021-04-14 16:18:32 +0900

    [2021.04.14] 문제 기록 업로드

---
+++ src/com/junsang/_20210414_NTS/Problem/_1_First.java
@@ -0,0 +1,14 @@
+package com.junsang._20210414_NTS.Problem;
+
+/**
+ * 조건
+ * - 2개의 입력을 받는다.
+ * - 문자를 왼쪽으로 회전시킴으로서, 두 개의 입력 문자가 같은지 판별한다.
+ * - 같으면 Ture, 같지 않으면 False 를 반환
+ */
+public class _1_First {
+
+    public static void main(String[] args) {
+        
+    }
+}
